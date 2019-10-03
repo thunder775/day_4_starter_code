b0eaefa61ba594bc82bbf3ae04926d176b9a7ebc
McLarenCollege <hello@mclarencollege.in>
2019-10-03 08:50:00 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.red,
+        appBar: AppBar(
+          title: Text('Roll the Dice'),
+          backgroundColor: Colors.red,
+        ),
+        body: Container(),
+      ),
+    ),
+  );
+}
